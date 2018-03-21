9fe5a84387164b9ad9dc1643740c2e8bf7669bb1
jdaguilar <jorgeaguilar2761@gmail.com>
2018-03-21 17:28:35 -0500

    commit

--- register.js
+++ register.js
@@ -119,6 +119,7 @@ function display_history(vector){
     var i;
     var code_html = "";
     for (i = 0; i < vector.lenght; i++){
+        console.log(i);
         //alert(i);
         //code_html += "<div>ID:" + vector[i].id + "latitude: " + vector[i].latitude + " longitude: " + vector[i].longitude  + "</div>";
         code_html += "<div>" + vector[i].datetime +"</div>";
