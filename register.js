4a20a32b741630d73f448851f01e0a54c076cbbc
jdaguilar <jorgeaguilar2761@gmail.com>
2018-03-21 17:10:24 -0500

    commit

--- register.js
+++ register.js
@@ -119,8 +119,9 @@ function display_history(vector){
     var i;
     var code_html = "";
     for (i = 0; i < vector.lenght; i++){
-        alert(i);
-        code_html += "<div>ID:" + vector[i].id + "latitude: " + vector[i].latitude + " longitude: " + vector[i].longitude  + "</div>";
+        //alert(i);
+        //code_html += "<div>ID:" + vector[i].id + "latitude: " + vector[i].latitude + " longitude: " + vector[i].longitude  + "</div>";
+        code_html += "<div>" + vector[i] +"</div>";
     }
     document.getElementById("history_section").innerHTML = code_html; 
 }
