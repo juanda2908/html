df27c6b4a302e53c16777db7ba083914a586ac37
jelambrar96 <jelambrar@gmail.com>
2018-03-20 23:42:17 -0500

    commit

--- register.js
+++ register.js
@@ -1,8 +1,13 @@
 //register.js
-
+var start_calendar; 
+var end_calendar;
 window.addEventListener("load", main, false); 
 
 function main(){
     
+    start_calendar = new CalendarPopup("start_calendar"); 
+    end_calendar = new CalendarPopup("end_calendar"); 
+    
 }
+
 
