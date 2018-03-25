50afed24e94018e885e2d3970248d3f026d0c31b
jdaguilar <jorgeaguilar2761@gmail.com>
2018-03-24 19:42:05 -0500

    After cache 5

--- register.js
+++ register.js
@@ -115,9 +115,13 @@ function send_button_function(){
     var data = JSON.parse(return_first);
     
     data.forEach(function(element){
-        console.log(element.id);
-        console.log(element.latitude);
-        console.log(element.longitude);
+
+        if(element !== undefined){
+            console.log(element.id);
+            console.log(element.latitude);
+            console.log(element.longitude);
+        }
+        
     });
     
 }
